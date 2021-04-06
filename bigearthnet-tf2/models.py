53d1d33d9345955e02a91b970573ddbf2f89f6d4
ngbountos <mpountos@outlook.com.gr>
2021-04-06 16:54:38 +0300

    Add Vision Transformer

--- bigearthnet-tf2/models.py
+++ bigearthnet-tf2/models.py
@@ -11,7 +11,7 @@ from tensorflow.keras.layers import Input, Add, Dense, Flatten, Activation, Lamb
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
-
+from tensorflow.keras import layers
 from tensorflow.keras.applications import ResNet50, ResNet101, ResNet152, VGG16, VGG19
 
 from tensorflow.python.ops import nn
@@ -40,7 +40,8 @@ MODELS_CLASS = {
     "EfficientNetB4": "EfficientNetB4",
     "EfficientNetB5": "EfficientNetB5",
     "EfficientNetB6": "EfficientNetB6",
-    "EfficientNetB7": "EfficientNetB7"
+    "EfficientNetB7": "EfficientNetB7",
+    "ViT":"ViT"
 }
 
 
@@ -744,3 +745,107 @@ class DNN_model(BigEarthModel):
                                                  'dropout_0' + resolution)
 
         return self.dropout(patches_concat_embed_, 0.5, 'dropout_0' + resolution)
+
+
+class Patches(tf.keras.layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+class PatchEncoder(tf.keras.layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = tf.keras.layers.Dense(units=projection_dim)
+        self.position_embedding = tf.keras.layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+class ViT(BigEarthModel):
+    def __init__(self, nb_class):
+        super().__init__(nb_class)
+
+    def _create_model_logits(self, allbands):
+        #num_bands = self._num_bands
+
+        #self.learning_rate = 0.001
+        #weight_decay = 0.0001
+        #batch_size = 256
+        #num_epochs = 100
+        image_size = 120  # We'll resize input images to this size
+        self.patch_size = 6  # Size of the patches to be extract from the input images
+        self.num_patches = (image_size // self.patch_size) ** 2
+        self.projection_dim = 64
+        self.num_heads = 4
+        self.transformer_units = [
+            self.projection_dim * 2,
+            self.projection_dim,
+        ]  # Size of the transformer layers
+        self.transformer_layers = 8
+        self.mlp_head_units = [2048, 1024]
+        x = self.create_vit_classifier(allbands)
+
+        return x
+
+    def mlp(self,x, hidden_units, dropout_rate):
+        for units in hidden_units:
+            x = layers.Dense(units, activation=tf.nn.gelu)(x)
+            x = layers.Dropout(dropout_rate)(x)
+        return x
+
+    def create_vit_classifier(self,allbands):
+        num_bands = self._num_bands
+        # Augment data.
+        #augmented = data_augmentation(inputs)
+        # Create patches.
+        patches = Patches(self.patch_size)(allbands)
+        # Encode patches.
+        encoded_patches = PatchEncoder(self.num_patches, self.projection_dim)(patches)
+
+        # Create multiple layers of the Transformer block.
+        for _ in range(self.transformer_layers):
+            # Layer normalization 1.
+            x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            # Create a multi-head attention layer.
+            attention_output = layers.MultiHeadAttention(
+                num_heads=self.num_heads, key_dim=self.projection_dim, dropout=0.1
+            )(x1, x1)
+            # Skip connection 1.
+            x2 = layers.Add()([attention_output, encoded_patches])
+            # Layer normalization 2.
+            x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+            # MLP.
+            x3 = self.mlp(x3, hidden_units=self.transformer_units, dropout_rate=0.1)
+            # Skip connection 2.
+            encoded_patches = layers.Add()([x3, x2])
+
+        # Create a [batch_size, projection_dim] tensor.
+        representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        representation = layers.Flatten()(representation)
+        representation = layers.Dropout(0.5)(representation)
+        # Add MLP.
+        features = self.mlp(representation, hidden_units=self.mlp_head_units, dropout_rate=0.5)
+        # Classify outputs.
+        #logits = layers.Dense(num_classes)(features)
+        # Create the Keras model.
+        #model = tf.keras.Model(inputs=allbands, outputs=features)
+        #model.summary()
+        return features
